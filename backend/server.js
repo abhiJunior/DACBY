a4e985d0b50a2bd1457e58f81497c7d158d325a9
Abhishek <mr.abhikush@gmail.com>
2026-05-08 12:42:03 +0530

    updated the cors url so the frontend deployed url can access web service

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://etharaai-6mrc.onrender.com"
+    "https://dacby.onrender.com"
   ],
   credentials: true
 }));
